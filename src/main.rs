ed5bf07ddcad6587622c38e4462bd0159e189b8f
hirotoe0112 <hirotoe0112@gmail.com>
2023-02-23 08:31:03 +0900

    配列とベクタ

--- src/main.rs
+++ src/main.rs
@@ -57,6 +57,7 @@ fn main() {
     // 上の変数と下の変数は異なるもの
     let t2 = (false, 1);
     // タプルをprintlnするには:?を記述する
+    // これをデバッグフォーマットという
     println!("{:?}", t1);
     // 要素を指定する場合はドットの後にインデックス番号を記述
     println!("{}", t1.0);
@@ -68,4 +69,63 @@ fn main() {
     // 値が何もない型のこと
     // 何も返さない関数の戻り値の型などで使用する
     let u = ();
+
+    // 配列
+    // 型に要素の型と数が含まれているため、一度作成すると要素数が変更できない
+    let arr1 = [1, 2, 3];
+    let arr2 = ["a", "b", "c"];
+    // "aaa"が100個並んだ配列
+    let arr3 = ["aaa"; 100];
+    // 配列をそのままprintlnする場合は:?を記述する
+    println!("{:?}", arr1);
+    println!("{}", arr1[2]);
+    let [_, yy, _] = arr1;
+    println!("{}", yy);
+    // 配列の一部分を切り出すことができる
+    // 切り出した配列をスライスという
+    // &をつけて開始位置と終了位置を指定（終了位置の要素は含まれない）
+    // 下の例では 1 ≦ x ＜ 2
+    // &は「参照」を表している（後に学習）
+    let slice = &arr2[1..2];
+    println!("{:?}", slice);
+    // 終了位置を含める場合は下のように記述する
+    let slice2 = &arr2[1..=2];
+    println!("{:?}", slice2);
+    // 開始位置、終了位置は省略できる
+    // 省略した場合はそれぞれ初めから、最後までとなる
+    // 両方省略した場合は配列全体となる
+    let slice3 = &arr2[..=1];
+    let slice4 = &arr2[1..];
+    println!("{:?}", slice3);
+    println!("{:?}", slice4);
+
+    // 後から要素数を変更できる配列＝ベクタ
+    let vec1 = vec![1, 2, 3, 4, 5, 6];
+    println!("{:?}", vec1);
+    println!("{:?}", &vec1[2..5]);
+    // デフォルトでは変更できないためmutを使う
+    // 下の例は空のベクタを作成している
+    let mut vec2 = Vec::new();
+    // 要素を追加
+    vec2.push("aaa");
+    println!("{:?}", vec2);
+    vec2.push("bbb");
+    println!("{:?}", vec2);
+    vec2.push("ccc");
+    vec2.push("ddd");
+    vec2.push("eee");
+    vec2.push("fff");
+    println!("{:?}", vec2);
+    // 末尾から1つ取得してベクタから消す
+    let pop = vec2.pop();
+    println!("{:?}", vec2);
+    println!("{:?}", pop);
+    // getを使用して値を取得できる
+    let get = vec2.get(2);
+    println!("{:?}", vec2);
+    println!("{:?}", get);
+    // Rustにはnullがなく代わりにOption型がある
+    // 存在しないインデックスを指定してもエラーにならずNoneが返る
+    let get2 = vec2.get(10000);
+    println!("{:?}", get2);
 }
